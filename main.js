44bd6ca8248b384c3653bff9f19bf387de679df6
Owen <ocotten24@basecampcodingacademy.org>
2023-10-06 11:06:19 -0500

    fixes

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-let button = document.querySelector('.btn')
-
+let button = document.querySelector('.darkmodebutton')
 
 button.addEventListener('click', () => {
   document.documentElement.classList.toggle('darkmode')
